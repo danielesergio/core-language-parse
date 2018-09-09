2ac93a24b0faa4a9533bad669b0057dd5f62f070
Daniele Sergio <daniele.sergio@kynetics.com>
2018-09-09 20:39:18 +0200

    Adds arguments to insert file path

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 stack build
 stack test
 stack build --exec core-language-parser-exe
-
+stack exec core-language-parser-exe -- "res/input.txt"
--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,11 @@
 module Main where
 
+import System.Environment
 import Lib
 import Read
 import Model
 
 main :: IO ()
-main =  readFileAndParse "res/input.txt" >>= print
+main =  do args <- getArgs
+           readFileAndParse (if (length args > 0) then head args else "res/input.txt") >>= print
 
